e34a53be0b9784236c248b5127054fb4b0602411
alvrodgar2 <alvrodgar2@alum.us.es>
2024-12-17 13:47:16 +0100

    Arreglar urls

--- practica_tres/urls.py
+++ practica_tres/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('', views.menu_principal, name='menu_principal'),
+    # path('cargar_base_datos/', views.cargar_base_datos, name='cargar_base_datos'),
+    # path('mostrar_registros/', views.mostrar_registros, name='mostrar_registros'),
+    # path('cargar_recsys/', views.cargar_recsys, name='cargar_recsys'),
+    path('animes_por_genero/', views.animes_por_genero, name='animes_por_genero'),
+    path('mejores_animes/', views.mejores_animes, name='mejores_animes'),
+    # path('recomendar_animes/', views.recomendar_animes, name='recomendar_animes'),
 ]
